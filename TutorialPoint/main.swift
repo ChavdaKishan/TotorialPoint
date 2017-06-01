de6165effc891c9be436d4e760d6f63bc77b6f3b
ChavdaKishan <chavdakish1996@gmail.com>
2017-06-01 11:49:23 +0530

    Initial Commit

---
+++ TutorialPoint/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TutorialPoint
+//
+//  Created by iFlame. on 01/06/17.
+//  Copyright © 2017 iFlame. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
